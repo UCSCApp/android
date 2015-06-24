7e56947b1aff2ca1de74a05395f9fe9d4a908097
isayyuhh <isaiahsolomon16@gmail.com>
2015-06-24 00:56:58 -0700

    events_c2
    - finished http call
    - working on image view and ui

--- app/src/main/java/slugapp/com/ucscstudentapp/event/Events.java
+++ app/src/main/java/slugapp/com/ucscstudentapp/event/Events.java
@@ -1,9 +1,14 @@
 package slugapp.com.ucscstudentapp.event;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,14 +24,103 @@ import slugapp.com.ucscstudentapp.R;
 
 public class Events extends AppCompatActivity {
 
+    private static final String SERVER_URL_PREFIX = "http://ec2-52-8-25-141.us-west-1.compute.amazonaws.com/events/get/v1";
+    public static final String PREF_POSTS = "pref_posts";
+    private ServerCall uploader;
+    private List<Event> events;
+
+    private class YourAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... args) {
+            // code where data is processing
+            // Then, we start the call.
+            PostMessageSpec my_call_spec = new PostMessageSpec();
+            my_call_spec.url = SERVER_URL_PREFIX;
+            my_call_spec.context = Events.this;
+            // Actual server call.
+            if (uploader != null) {
+                // There was already an upload in progress.
+                uploader.cancel(true);
+            }
+            uploader = new ServerCall();
+            uploader.execute(my_call_spec);
+            return null;
+        }
+    }
+
+    /**
+     * This class is used to do the HTTP call, and it specifies how to use the result.
+     */
+    class PostMessageSpec extends ServerCallSpec {
+        @Override
+        public void useResult(Context context, String result) {
+            if (result == null) {
+                // Do something here, e.g. tell the user that the server cannot be contacted.
+                //Log.i(LOG_TAG, "The server call failed.");
+            } else {
+                // Translates the string result, decoding the Json.
+                //Log.i(LOG_TAG, "Received string: " + result);
+                if(result != null || result != "")
+                    displayResult(result);
+                // Stores in the settings the last messages received.
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString(PREF_POSTS, result);
+                editor.commit();
+            }
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events);
+        events = new ArrayList<Event>();
         linkActionBar("Events");
-        List<Event> events = parseFakeData();
-        Log.e("STring 2", "" + events.size());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Let us display the previous posts, if any.
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String result = settings.getString(PREF_POSTS, null);
+        if (result != null) {
+            displayResult(result);
+        }
+        new YourAsyncTask().execute();
         linkListView(events);
+    }
+
+    @Override
+    protected void onPause() {
+        // Stops the upload if any.
+        if (uploader != null) {
+            uploader.cancel(true);
+            uploader = null;
+        }
+        super.onPause();
+    }
+
+    private void displayResult(String json) {
+        events.clear();
+        try {
+            JSONArray arr = new JSONArray(json);
+            Log.e("HERE", "" + arr.length());
+            for(int i = 0; i < arr.length(); ++i) {
+                JSONObject obj = arr.getJSONObject(i);
+                Event e = new Event(obj.getString("name"),
+                        obj.getString("date"),
+                        obj.getString("description"));//,
+                        //obj.getString("url"));
+                events.add(e);
+            }
+
+        }catch(JSONException je) {
+            Log.e("string", "" + je.getMessage());
+        }
     }
 
     private void linkActionBar(String name) {
@@ -40,25 +134,5 @@ public class Events extends AppCompatActivity {
     private void linkListView(List<Event> events) {
         ListView lv = (ListView) findViewById(R.id.list);
         lv.setAdapter(new EventsAdapter(this, events));
-    }
-
-    private List<Event> parseFakeData() {
-        String json = "[\n       {\n          \"name\":\"Edge of Eden\",\n          \"date\":\"July 18th 10pm 2am\",\n          \"description\":\"Musical festival with a finale by Squidward Tortellini\"\n       },\n       {\n          \"name\":\"Holi Festival\",\n          \"date\":\"May 25th 10am 12pm\",\n          \"description\":\"This event is super fun and super great!\"\n       },\n       {\n          \"name\":\"Meeting\",\n          \"date\":\"May 31st 2pm 4pm\",\n          \"description\":\"Simba is asking you to go to this super boring meeting thing again\"\n       },\n       {\n          \"name\":\"French Fried\",\n          \"date\":\"January 1st 13am 7pm\",\n          \"description\":\"TBH Im not entirely sure what this is, so please dont come to this event at all, This desciption is purposely awkwardly long to hopefully break all of your apps becase I am a devious motherfucker like that. So I am still writing random shit now to break your apps in my malicious ways. I wonder if anyone will actually handle this case. I sure as hell wouldnt. Who the fuck actually spends the time to write this long a description about a dumb event called French Fried holy fuck. Btw I hope you arent actually reading this when you should be coding the solution to fixing your description box that just broke due to my malicious test script. Unless of course it worked in which case, you should probably stop reading this anyway because holy fuck this is a long motha fucking description. It is called French Fried. Starts at 13am. Be there.\"\n       },\n       {\n          \"name\":\"Short but sweet\",\n          \"date\":\"never\",\n          \"description\":\"meow\"\n       }\n    ]";
-        List<Event> rtrn = new ArrayList<>();
-        try {
-            JSONArray arr = new JSONArray(json);
-            Log.e("HERE", "" + arr.length());
-            for(int i = 0; i < arr.length(); ++i) {
-                JSONObject obj = arr.getJSONObject(i);
-                Event e = new Event(obj.getString("name"),
-                                    obj.getString("date"),
-                                    obj.getString("description"));
-                rtrn.add(e);
-            }
-
-        }catch(JSONException je) {
-            Log.e("string", "" + je.getMessage());
-        }
-        return rtrn;
     }
 }
