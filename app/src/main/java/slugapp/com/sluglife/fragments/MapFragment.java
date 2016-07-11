24e5a376a62d099e89f5031973a8f00f0b643ac3
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2016-07-10 19:55:52 -0700

    added permission check for location

--- app/src/main/java/slugapp/com/sluglife/fragments/MapFragment.java
+++ app/src/main/java/slugapp/com/sluglife/fragments/MapFragment.java
@@ -12,6 +12,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -113,15 +114,11 @@ public class MapFragment extends BaseMapFragment {
                 ActivityCompat.checkSelfPermission(
                         this.mContext, Manifest.permission.ACCESS_COARSE_LOCATION) !=
                         PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
         }
-        map.setMyLocationEnabled(true);
+        else map.setMyLocationEnabled(true);
     }
 
     @Override
